83d75924ee495536c17a330abb529e19378ac90d
aondrej ryan <aondrejryan@gmail.com>
2023-01-22 00:41:07 -0800

    local storage items display on user device

--- js/main.js
+++ js/main.js
@@ -10,25 +10,60 @@ function getFetch(){
       .then(data => {
         console.log(data)
         console.log(data.desc)
-        const li = document.createElement('li')
-        li.innerText = data.name
-        li.id = data.name
+        // this conditional checks if user entered the correct spell if not alerts an error or displays spell is correc
+        if(data['name'] !== undefined){
+          const li = document.createElement('li')
+          li.innerText = data.name
+          li.id = data.name
 
-        let spells = {
-            nameOf: data.name,
-            des: data.desc,
-          }
-          spellList.push(spells)
-          console.log(spellList)
-          localStorage.setItem('spells', JSON.stringify(spellList))
-        document.querySelector('ul').appendChild(li)
+          // creates objects with the spells info to append to spellList array for local storage
+          let spells = {
+              nameOf: data.name,
+              des: data.desc,
+            }
+            // pushing it to the the arraty
+            spellList.push(spells)
+            
+            // setting up the local storage 
+            localStorage.setItem('spells', JSON.stringify(spellList))
+
+            //appending spells to the dom 
+            document.querySelector('ul').appendChild(li)
+        } else {
+          alert(`Error you didn't enter a spell`)
+          document.querySelector('input').value = ''
+        }
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 }
+// creratign a class to display interface 
+class Interface{
+  constructor(){
+  }
+      // this function will display spell data that user previously entered 
+      displaySpells(){
+        // this conditionally will run on page load
+        // to see if theres been spells entered into the loal storage 
+          if(spellList.length > 0){
+            spellList.forEach(element => {
+              // the following code will be similar to what we're doing in the api call with appending elements
+              const ul = document.querySelector('ul')
+              const li = document.createElement('li')
+              li.id = element['nameOf']
+              li.innerText = element['nameOf']
+              ul.appendChild(li)
+            })
+          }
+      }
 
-
+      showDes(){
+        
+      }
+}
+const ui = new Interface()
+ui.displaySpells()
 //local storage
 
 
