25274083f30b082987dc7154565d318a6c239fed
Richard-Edwards-Dev <edwardsrichardk@gmail.com>
2023-01-22 18:56:44 -0800

    OOP refactor and separated the search and add functions

--- js/main.js
+++ js/main.js
@@ -1,42 +1,10 @@
 //Example fetch using DnD5eAPI - place subclasses in ul
-document.querySelector('button').addEventListener('click', getFetch)
-let spellList = JSON.parse(localStorage.getItem('spells')) || []
+
+let spellList = JSON.parse(localStorage.getItem('spells')) || [];
+//holds the currently searched spell
+let currentSpell
 function getFetch(){
-  const choice = document.querySelector('input').value.replaceAll(' ', '-')
-  const url = `https://www.dnd5eapi.co/api/spells/${choice}`
 
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-        console.log(data.desc)
-        // this conditional checks if user entered the correct spell if not alerts an error or displays spell is correc
-        if(data['name'] !== undefined){
-          const li = document.createElement('li')
-          li.innerText = data.name
-          li.id = data.name
-
-          // creates objects with the spells info to append to spellList array for local storage
-          let spells = {
-              nameOf: data.name,
-              des: data.desc,
-            }
-            // pushing it to the the arraty
-            spellList.push(spells)
-            
-            // setting up the local storage 
-            localStorage.setItem('spells', JSON.stringify(spellList))
-
-            //appending spells to the dom 
-            document.querySelector('ul').appendChild(li)
-        } else {
-          alert(`Error you didn't enter a spell`)
-          document.querySelector('input').value = ''
-        }
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
 }
 // creratign a class to display interface 
 class Interface{
@@ -54,32 +22,72 @@ class Interface{
               li.id = element['nameOf']
               li.innerText = element['nameOf']
               ul.appendChild(li)
+              //adds Event listener to Each
+              document.querySelector(`#${li.id}`).addEventListener('click', ui.showDes)
             })
           }
       }
+      //This function makes the request to the api to search for the spell name that was entered
+      searchSpells(){
+        const choice = document.querySelector('input').value.replaceAll(' ', '-')
+        const url = `https://www.dnd5eapi.co/api/spells/${choice}`
+      
+        fetch(url)
+            .then(res => res.json()) // parse response as JSON
+            .then(data => {
+              // this conditional checks if user entered the correct spell if not alerts an error or displays spell is correc
+              if(data['name'] !== undefined){
+                document.querySelector('h2').innerText = data.name
+                document.querySelector('p').innerText = data.desc                  
+              // creates objects with the spells info to append to spellList array for local storage
+                let spells = {
+                    nameOf: data.name,
+                    des: data.desc,
+                  }
+                  currentSpell = spells
+                  console.log(currentSpell.nameOf)
+              } else {
+                alert(`Error you didn't enter a spell`)
+                document.querySelector('input').value = ''
+              }
+            })
+            .catch(err => {
+                console.log(`error ${err}`)
+            });
 
-      showDes(){
-        
+      }
+      //this function adds the currently searched spell to your list and commits it to local storage
+      addSpelltoList(){
+        const li = document.createElement('li')
+        li.innerText = currentSpell.nameOf
+        li.id = currentSpell.nameOf
+        // pushing it to the the arraty
+        spellList.push(currentSpell)
+                  
+        // setting up the local storage 
+        localStorage.setItem('spells', JSON.stringify(spellList))
+
+        //appending spells to the dom 
+        document.querySelector('ul').appendChild(li)
+
+         //adds Event listener to new spell
+         document.querySelector(`#${li.id}`).addEventListener('click', ui.showDes)
+
+      }
+      //this function will eventuall print the spell description to the DOM. maybe by toggling a 'hidden' css class?
+      //for now it searches through the array of spells and consoleLogs the decription
+      showDes(click){
+        for (let spell of spellList){
+          if(spell.nameOf === click.target.id){
+            console.log(spell.des)
+          }
+        }
       }
 }
-const ui = new Interface()
+const ui = new Interface('')
 ui.displaySpells()
-//local storage
+document.querySelector('#searchSpells').addEventListener('click', ui.searchSpells)
+document.querySelector('#addToList').addEventListener('click', ui.addSpelltoList)
 
 
-// document.querySelector('button').addEventListener('click', anothaOne)
 
-// function anothaOne(){
-//     let botScore = Number(localStorage.getItem('botScore'))
-//     botScore = botScore + 1
-//     localStorage.setItem('botScore', botScore)
-// }
-// if(!localStorage.getItem('spells')){
-//     localStorage.setItem('spells', ` ${data.index} `)
-// } else {
-// let spellList = localStorage.getItem('spells')
-//     if(!spellList.includes(` ${data.index} `)){
-//     spellList += ` ${data.index} `
-//     localStorage.setItem('spells', spellList)
-//     }
-// }
