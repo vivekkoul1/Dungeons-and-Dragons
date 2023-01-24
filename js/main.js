51d131dcff41ac9768de49e0963604429da63f89
Richard-Edwards-Dev <edwardsrichardk@gmail.com>
2023-01-23 19:20:28 -0800

    added a clear spells function

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
-//Example fetch using DnD5eAPI - place subclasses in ul
 
 let spellList = JSON.parse(localStorage.getItem('spells')) || [];
 //holds the currently searched spell
 
-function getFetch(){
-
-}
 // creratign a class to display interface 
 class Interface{
   constructor(){
@@ -36,7 +32,7 @@ class Interface{
   }
   //This function makes the request to the api to search for the spell name that was entered
   async searchSpells() {
-    const choice = document.querySelector('input').value.replaceAll(' ', '-')
+    const choice = document.querySelector('input').value.replaceAll(' ', '-').toLowerCase()
     const url = `https://www.dnd5eapi.co/api/spells/${choice}`
     //async/await syntax to make more readable
     const res = await fetch(url) // parse response as JSON
@@ -107,13 +103,18 @@ class Interface{
     console.log(click.target.id)
     document.querySelector(`.${click.target.id}desc`).classList.toggle('hidden')
   }
-
+  emptyList(){
+    localStorage.clear()
+    spellList = []
+    ui.displaySpells()
+  }
 }
 const ui = new Interface('')
 //populates the list from storage
 ui.displaySpells()
 document.querySelector('#searchSpells').addEventListener('click', ui.searchSpells)
 document.querySelector('#addToList').addEventListener('click', ui.addSpelltoList)
+document.querySelector('#clearList').addEventListener('click', ui.emptyList)
 
 
 
